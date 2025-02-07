875a8693878ffc0dd69fa2a34601d7d606e7652a
PronPan <gerbildude124@gmail.com>
2025-02-06 18:48:19 -0800

    Update nginxproxymanager.sh

--- ct/nginxproxymanager.sh
+++ ct/nginxproxymanager.sh
@@ -32,12 +32,6 @@ function update_script() {
     msg_error "No ${APP} Installation Found!"
     exit
   fi
-  if ! command -v pnpm &> /dev/null; then  
-    msg_info "Installing pnpm"
-    #export NODE_OPTIONS=--openssl-legacy-provider
-    npm install -g pnpm@8.15 &>/dev/null
-    msg_ok "Installed pnpm"
-  fi
   RELEASE=$(curl -s https://api.github.com/repos/NginxProxyManager/nginx-proxy-manager/releases/latest |
     grep "tag_name" |
     awk '{print substr($2, 3, length($2)-4) }')
